75e84bd0eaa4f960115e39325425231f15b2a304
Clement <clement.capart@gmail.com>
2017-07-30 16:13:58 +0100

    Initial commit

---
+++ Assets/Example/SayHelloGameSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SayHelloGameSystem : GameSystem
+{
+	public string m_TextToDisplay = "Hello, world!";
+	private string m_ActualTextToDisplay = "";
+
+	public override void Init()
+	{
+		m_ActualTextToDisplay = m_TextToDisplay;
+	}
+
+	public override void RecoverAfterRecompile()
+	{
+		// - To recover things you couldn't serialize,
+		// - That is a silly example but...!
+		m_ActualTextToDisplay = m_TextToDisplay;
+	}
+
+	public override void Update()
+	{
+		Debug.Log(m_ActualTextToDisplay);
+	}
+}
---
+++ Assets/Example/YourGameSystemsManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YourGameSystemsManager : GameSystemsManager
+{
+	protected override void AddAdditionalGameSystems()
+	{
+		// Add your own game systems here
+		AddGameSystem(GameSystem.CreateInstance<SayHelloGameSystem>());
+		AddGameSystem(GameSystem.InstantiateFromAsset("SomePrefabSpawnerGameSystem"));
+	}
+}
